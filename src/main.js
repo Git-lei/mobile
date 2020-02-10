e51c22d77178227ee156623f0e01638da79a1879
Git-lei <Fluoxetine_Si@163.com>
2020-02-10 14:38:14 +0800

    布局|| 配置路由

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//导入 amfe-flexible
+import 'amfe-flexible/index.min.js'
 Vue.config.productionTip = false
-
+Vue.use(Vant),
 new Vue({
+ 
   router,
   store,
   render: h => h(App)
